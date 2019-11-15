31d6e123c1feac12318a5ee582b9dbb8b1fa539c
Aidar <brisingr@inbox.ru>
2019-11-15 14:36:37 +0600

    Initial Commit

---
+++ TestTaskDisplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskDisplay
+//
+//  Created by Мак on 11/15/19.
+//  Copyright © 2019 Aidar Zhussupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
